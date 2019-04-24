289ee2050474be289100f1f99cf06637a626f401
joshuabyker <47902849+joshuabyker@users.noreply.github.com>
2019-04-24 16:02:55 -0700

    initial

---
+++ assets/train.js
@@ -0,0 +1,12 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCHb6iFAzM5l9ZUC_BA8iYvA1lclKw5TF8",
+    authDomain: "train-scheduler-60ac4.firebaseapp.com",
+    databaseURL: "https://train-scheduler-60ac4.firebaseio.com",
+    projectId: "train-scheduler-60ac4",
+    storageBucket: "train-scheduler-60ac4.appspot.com",
+    messagingSenderId: "895916806591"
+  };
+  firebase.initializeApp(config);
+</script>
